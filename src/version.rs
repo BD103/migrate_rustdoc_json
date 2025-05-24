edb242a18c0965f77a586a2a368f724a79ac9a32
BD103 <59022059+BD103@users.noreply.github.com>
2025-05-24 10:30:43 -0400

    feat: add docs to `version` module

--- src/version.rs
+++ src/version.rs
@@ -1,10 +1,15 @@
 use serde::Deserialize;
 
+/// A simplified version of `rustdoc_types::Crate` that only has the `format_version` field.
+/// 
+/// This type is intended to be independent of the format version so that it may deserialize any
+/// version of `rustdoc`'s JSON output.
 #[derive(Deserialize)]
 struct Crate {
     pub format_version: u32,
 }
 
+/// Detects the format version from `rustdoc`'s JSON output.
 pub fn detect_version(json: &str) -> u32 {
     let Crate { format_version } = serde_json::from_str(json).unwrap();
     format_version
