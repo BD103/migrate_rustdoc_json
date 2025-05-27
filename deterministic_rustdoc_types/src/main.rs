6a267276f3fe39754db7e1c8a231a0012da75f19
BD103 <59022059+BD103@users.noreply.github.com>
2025-05-27 14:13:27 -0400

    feat: setup json loading and writing infra

--- deterministic_rustdoc_types/src/main.rs
+++ deterministic_rustdoc_types/src/main.rs
@@ -1,7 +1,20 @@
+use anyhow::Context;
+use serde_json::Value;
+
 mod args;
 
 fn main() -> anyhow::Result<()> {
     let args = self::args::parse_args()?;
+
+    let input = std::fs::read_to_string(&args.input)
+        .with_context(|| format!("could not read `--input` file: {}", args.input.display()))?;
+
+    // `Value` uses a `BTreeMap` that sorts all keys on creation.
+    let value: Value = serde_json::from_str(&input)?;
+
+    let out = serde_json::to_string(&value)?;
+
+    println!("{out}");
 
     Ok(())
 }
