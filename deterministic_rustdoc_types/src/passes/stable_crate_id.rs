737ed4d95369e6870e1e7dbebf9c984b9923f424
BD103 <59022059+BD103@users.noreply.github.com>
2025-05-28 09:58:28 -0400

    feat: rewrite `stable_crate_id` pass

--- deterministic_rustdoc_types/src/passes/stable_crate_id.rs
+++ deterministic_rustdoc_types/src/passes/stable_crate_id.rs
@@ -1,83 +1,73 @@
-use std::{collections::BTreeMap, mem};
+use std::collections::BTreeMap;
 
-use anyhow::Context;
 use serde_json::{Map, Number, Value};
 
-#[derive(Debug)]
-struct ExternalCrates(Map<String, Value>);
+pub fn pass(crate_: &mut Value) {
+    let Value::Object(old_external_crates) = crate_["external_crates"].take() else {
+        panic!()
+    };
 
-impl ExternalCrates {
-    pub fn take_from_crate(crate_: &mut Value) -> anyhow::Result<Self> {
-        let Value::Object(ref mut reference) = crate_["external_crates"] else {
-            anyhow::bail!("`external_crates` is not a map");
-        };
+    // A sorted list of crate names and their old IDs. This cannot be a `BTreeMap` because there
+    // may be duplicate crate names.
+    let mut sorted_old_ids = Vec::with_capacity(old_external_crates.len());
 
-        let external_crates = mem::replace(reference, Map::new());
-
-        Ok(Self(external_crates))
-    }
-}
-
-#[derive(Debug)]
-struct CrateMap(BTreeMap<u32, u32>);
-
-impl CrateMap {
-    pub fn from_external_crates(external_crates: &ExternalCrates) -> anyhow::Result<Self> {
-        let mut sorted_ids = BTreeMap::new();
-
-        for (id, external_crate) in external_crates.0.iter() {
-            let Some(name) = external_crate["name"].as_str().map(str::to_string) else {
-                anyhow::bail!("could not extract `name` from `external_crates`");
-            };
-
-            let id = id
-                .parse::<u32>()
-                .context("could not parse crate id as a `u32`")?;
-
-            sorted_ids.insert(name, id);
-        }
-
-        let mut crate_map = BTreeMap::new();
-
-        for i in sorted_ids.into_values().enumerate() {
-            let (new_id, old_id) = (i.0 as u32 + 1, i.1);
-            crate_map.insert(old_id, new_id);
-        }
-
-        Ok(Self(crate_map))
+    for (old_id, external_crate) in old_external_crates.iter() {
+        let crate_name = external_crate["name"].as_str().unwrap();
+        let old_id = old_id.parse::<u32>().unwrap();
+        let html_root_url = external_crate["html_root_url"].as_str();
+        sorted_old_ids.push((crate_name, html_root_url, old_id));
     }
 
-    pub fn get(&self, old_id: u32) -> Option<u32> {
-        eprintln!("GET: {old_id}");
-        self.0.get(&old_id).copied()
-    }
-}
-
-pub fn pass(crate_: &mut Value) -> anyhow::Result<()> {
-    let external_crates = ExternalCrates::take_from_crate(crate_)?;
-    let crate_map = dbg!(CrateMap::from_external_crates(&external_crates)?);
-
-    let new_external_crates: Map<String, Value> = external_crates
-        .0
-        .into_iter()
-        .map(|(k, v)| {
-            let k = k.parse::<u32>().unwrap();
-            let new_k = crate_map.get(k).unwrap().to_string();
-            (new_k, v)
+    // Stable sort by the crate names, then HTML URL. If both are the same, we sort by old ID,
+    // which may hurt determinism.
+    sorted_old_ids.sort_by(|a, b| {
+        a.0.cmp(b.0).then_with(|| a.1.cmp(&b.1)).then_with(|| {
+            eprintln!("WARNING: Found two identical crates named {}. Sorting by old ID, which may hurt determinism.", a.0);
+            a.2.cmp(&b.2)
         })
-        .collect();
+    });
+
+    // Maps old IDs to new IDs.
+    let mut id_map = BTreeMap::new();
+
+    for (i, (_, _, old_id)) in sorted_old_ids.into_iter().enumerate() {
+        let new_id = i as u32 + 1;
+        id_map.insert(old_id, new_id);
+    }
+
+    let mut new_external_crates = Map::with_capacity(old_external_crates.len());
+
+    for (old_id, external_crate) in old_external_crates {
+        let old_id = old_id.parse::<u32>().unwrap();
+
+        let new_id = id_map
+            .get(&old_id)
+            .expect(&format!("no entry for key {old_id}"));
+
+        new_external_crates.insert(new_id.to_string(), external_crate);
+    }
 
     crate_["external_crates"] = Value::Object(new_external_crates);
 
     for (_, item) in crate_["index"].as_object_mut().unwrap() {
-        let new_id = crate_map.get(item["crate_id"].as_u64().unwrap() as u32).unwrap();
+        let old_id = item["crate_id"].as_u64().unwrap() as u32;
+
+        if old_id == 0 {
+            continue;
+        }
+
+        let new_id = id_map[&old_id];
         item["crate_id"] = Value::Number(Number::from(new_id));
     }
 
     for (_, item_summary) in crate_["paths"].as_object_mut().unwrap() {
-        let new_id = crate_map.get(item_summary["crate_id"].as_u64().unwrap() as u32).unwrap();
+        let old_id = item_summary["crate_id"].as_u64().unwrap() as u32;
+
+        if old_id == 0 {
+            continue;
+        }
+
+        let new_id = id_map[&old_id];
         item_summary["crate_id"] = Value::Number(Number::from(new_id));
     }
-
-    Ok(())
 }
