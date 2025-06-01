62475e6681bd8a21e5107ff513275413a1eb5341
BD103 <59022059+BD103@users.noreply.github.com>
2025-06-01 00:09:31 -0400

    feat: setup repr parsing infra

--- src/migrations/v42.rs
+++ src/migrations/v42.rs
@@ -1,15 +1,58 @@
 //! <https://github.com/rust-lang/rustdoc-types/blob/trunk/CHANGELOG.md#v0390---2025-03-24>
-//!
-//! Currently attributes are left as-is in their debug format. We may be able to reformat them into
-//! their pretty-printed variants.
 
 use rustdoc_types_42 as current;
 use rustdoc_types_43 as up;
 
-use crate::{declare_migrate_up, declare_serialize_deserialize};
+use crate::{declare_migrate_up, declare_serialize_deserialize, traits::MigrateUp};
 
 declare_migrate_up!(42, 43);
 declare_serialize_deserialize!();
+
+impl MigrateUp for current::Item {
+    type Up = up::Item;
+
+    fn migrate_up(self) -> Self::Up {
+        let current::Item {
+            id,
+            crate_id,
+            name,
+            span,
+            visibility,
+            docs,
+            links,
+            attrs,
+            deprecation,
+            inner,
+        } = self;
+
+        let attrs = attrs.into_iter().map(pretty_print_attr).collect();
+
+        up::Item {
+            id: id.migrate_up(),
+            crate_id,
+            name,
+            span: span.migrate_up(),
+            visibility: visibility.migrate_up(),
+            docs,
+            links: links.migrate_up(),
+            attrs,
+            deprecation: deprecation.migrate_up(),
+            inner: inner.migrate_up(),
+        }
+    }
+}
+
+fn pretty_print_attr(debug_printed: String) -> String {
+    let Some(reprs) = debug_printed.strip_prefix("#[attr = Repr([") else {
+        return debug_printed;
+    };
+
+    let Some(reprs) = reprs.strip_suffix("])]\n") else {
+        return debug_printed;
+    };
+
+    reprs.to_string()
+}
 
 crate::impl_unchanged_migrations! {
     Crate,
@@ -28,7 +71,6 @@ crate::impl_unchanged_migrations! {
     Generics,
     Id,
     Impl,
-    Item,
     ItemSummary,
     Module,
     Path,
