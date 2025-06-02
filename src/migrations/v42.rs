9a7a4e8e9c63b5916cad83e6c99bb5b637237c99
BD103 <59022059+BD103@users.noreply.github.com>
2025-06-02 18:49:53 -0400

    feat(v42): create simple ast to avoid allocating `String`s

--- src/migrations/v42.rs
+++ src/migrations/v42.rs
@@ -145,12 +145,79 @@ crate::impl_unchanged_migrations! {
 }
 
 mod parser {
+    use std::fmt;
+
     use winnow::{
         Parser,
         ascii::{alpha1, alphanumeric1, digit1},
         combinator::{delimited, dispatch, empty, fail, separated},
         error::Result,
     };
+
+    #[derive(Clone)]
+    enum ReprAttr<'s> {
+        Int(IntType),
+        Rust,
+        C,
+        Packed(&'s str),
+        Simd,
+        Transparent,
+        Align(&'s str),
+        Empty,
+    }
+
+    impl<'s> fmt::Display for ReprAttr<'s> {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            match self {
+                Self::Int(int_type) => write!(f, "{int_type}"),
+                Self::Rust => write!(f, "Rust"),
+                Self::C => write!(f, "C"),
+                Self::Packed(align) => write!(f, "packed({align})"),
+                Self::Simd => write!(f, "simd"),
+                Self::Transparent => write!(f, "transparent"),
+                Self::Align(align) => write!(f, "align({align})"),
+                Self::Empty => Err(fmt::Error),
+            }
+        }
+    }
+
+    #[derive(Clone)]
+    enum IntType {
+        Signed(IntSize),
+        Unsigned(IntSize),
+    }
+
+    impl fmt::Display for IntType {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            match self {
+                Self::Signed(int_size) => write!(f, "i{int_size}"),
+                Self::Unsigned(int_size) => write!(f, "u{int_size}"),
+            }
+        }
+    }
+
+    #[derive(Clone)]
+    enum IntSize {
+        XSize,
+        X8,
+        X16,
+        X32,
+        X64,
+        X128,
+    }
+
+    impl fmt::Display for IntSize {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            match *self {
+                IntSize::XSize => write!(f, "size"),
+                IntSize::X8 => write!(f, "8"),
+                IntSize::X16 => write!(f, "16"),
+                IntSize::X32 => write!(f, "32"),
+                IntSize::X64 => write!(f, "64"),
+                IntSize::X128 => write!(f, "128"),
+            }
+        }
+    }
 
     pub fn parse_repr_attrs(input: &mut &str) -> Result<Option<String>> {
         let mut reprs: Vec<_> = delimited(
@@ -160,64 +227,65 @@ mod parser {
         )
         .parse_next(input)?;
 
-        // Only keep non-empty strings, thus removing an `ReprEmpty`s.
-        reprs.retain(|s| !s.is_empty());
+        // Only keep non-empty reprs.
+        reprs.retain(|s| !matches!(s, ReprAttr::Empty));
 
         // If there are no reprs, usually because they got filtered out, return `None`.
         if reprs.is_empty() {
             return Ok(None);
         }
 
+        let reprs: Vec<_> = reprs.into_iter().map(|repr| format!("{repr}")).collect();
+
         Ok(Some(format!("#[repr({})]", reprs.join(", "))))
     }
 
-    fn parse_repr_attr(input: &mut &str) -> Result<String> {
+    fn parse_repr_attr<'s>(input: &mut &'s str) -> Result<ReprAttr<'s>> {
         dispatch! { alpha1;
-            "ReprInt" => delimited("(", parse_int_type, ")").map(String::from),
-            "ReprRust" => empty.value(String::from("Rust")),
-            "ReprC" => empty.value(String::from("C")),
-            "ReprPacked" => delimited("(", parse_align, ")").map(|s| format!("packed({s})")),
-            "ReprSimd" => empty.value(String::from("simd")),
-            "ReprTransparent" => empty.value(String::from("transparent")),
-            "ReprAlign" => delimited("(", parse_align, ")").map(|s| format!("align({s})")),
-            // This shouldn't realistically ever be hit. If it is, we return an empty string.
-            // (`parse_repr_attrs()` has custom logic for detecting this and removing it.)
-            "ReprEmpty" => empty.default_value(),
+            "ReprInt" => delimited("(", parse_int_type, ")").map(|int_type| ReprAttr::Int(int_type)),
+            "ReprRust" => empty.value(ReprAttr::Rust),
+            "ReprC" => empty.value(ReprAttr::C),
+            "ReprPacked" => delimited("(", parse_align, ")").map(|s| ReprAttr::Packed(s)),
+            "ReprSimd" => empty.value(ReprAttr::Simd),
+            "ReprTransparent" => empty.value(ReprAttr::Transparent),
+            "ReprAlign" => delimited("(", parse_align, ")").map(|s| ReprAttr::Align(s)),
+            // This shouldn't realistically ever be hit.
+            "ReprEmpty" => empty.value(ReprAttr::Empty),
             _ => fail::<_, _, _>,
         }
         .parse_next(input)
     }
 
-    fn parse_int_type<'s>(input: &mut &'s str) -> Result<&'s str> {
+    fn parse_int_type<'s>(input: &mut &'s str) -> Result<IntType> {
         dispatch! { alpha1;
-            "SignedInt" => delimited("(", parse_int_ty, ")"),
-            "UnsignedInt" => delimited("(", parse_uint_ty, ")"),
+            "SignedInt" => delimited("(", parse_int_ty, ")").map(|int_size| IntType::Signed(int_size)),
+            "UnsignedInt" => delimited("(", parse_uint_ty, ")").map(|int_size| IntType::Unsigned(int_size)),
             _ => fail::<_, _, _>,
         }
         .parse_next(input)
     }
 
-    fn parse_int_ty<'s>(input: &mut &'s str) -> Result<&'s str> {
+    fn parse_int_ty<'s>(input: &mut &'s str) -> Result<IntSize> {
         dispatch! { alphanumeric1;
-            "Isize" => empty.value("isize"),
-            "I8" => empty.value("i8"),
-            "I16" => empty.value("i16"),
-            "I32" => empty.value("i32"),
-            "I64" => empty.value("i64"),
-            "I128" => empty.value("i128"),
+            "Isize" => empty.value(IntSize::XSize),
+            "I8" => empty.value(IntSize::X8),
+            "I16" => empty.value(IntSize::X16),
+            "I32" => empty.value(IntSize::X32),
+            "I64" => empty.value(IntSize::X64),
+            "I128" => empty.value(IntSize::X128),
             _ => fail::<_, _, _>,
         }
         .parse_next(input)
     }
 
-    fn parse_uint_ty<'s>(input: &mut &'s str) -> Result<&'s str> {
+    fn parse_uint_ty<'s>(input: &mut &'s str) -> Result<IntSize> {
         dispatch! { alphanumeric1;
-            "Usize" => empty.value("usize"),
-            "U8" => empty.value("u8"),
-            "U16" => empty.value("u16"),
-            "U32" => empty.value("u32"),
-            "U64" => empty.value("u64"),
-            "U128" => empty.value("u128"),
+            "Usize" => empty.value(IntSize::XSize),
+            "U8" => empty.value(IntSize::X8),
+            "U16" => empty.value(IntSize::X16),
+            "U32" => empty.value(IntSize::X32),
+            "U64" => empty.value(IntSize::X64),
+            "U128" => empty.value(IntSize::X128),
             _ => fail::<_, _, _>,
         }
         .parse_next(input)
