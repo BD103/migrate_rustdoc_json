bbf7c1c7a445c5b06079053dc99618293054215c
BD103 <59022059+BD103@users.noreply.github.com>
2025-06-11 19:20:36 -0400

    feat: use `dyn Any` in `UntypedCrate`
    This removes the unsafe code and stops `UntypedCrate`s from potentially leaking.

--- src/traits.rs
+++ src/traits.rs
@@ -8,29 +8,3 @@ pub trait MigrateUp {
     /// Migrates `self` to the new version.
     fn migrate_up(self) -> Self::Up;
 }
-
-/// Represents the `Crate` type from `rustdoc_types`.
-pub trait Crate {
-    /// The format version of this `Crate`.
-    const FORMAT_VERSION: u32;
-}
-
-impl Crate for rustdoc_types_41::Crate {
-    const FORMAT_VERSION: u32 = 41;
-}
-
-impl Crate for rustdoc_types_42::Crate {
-    const FORMAT_VERSION: u32 = 42;
-}
-
-impl Crate for rustdoc_types_43::Crate {
-    const FORMAT_VERSION: u32 = 43;
-}
-
-impl Crate for rustdoc_types_44::Crate {
-    const FORMAT_VERSION: u32 = 44;
-}
-
-impl Crate for rustdoc_types_45::Crate {
-    const FORMAT_VERSION: u32 = 45;
-}
--- src/untyped_crate.rs
+++ src/untyped_crate.rs
@@ -1,23 +1,13 @@
-use crate::traits::Crate;
+use std::any::Any;
 
-/// A safe, untyped, heap-allocated `Crate` with an associated format version.
-///
-/// Note that dropping this type without calling [`UntypedCrate::into_crate()`] will result in a
-/// memory leak.
-pub struct UntypedCrate {
-    crate_: *mut (),
-    format_version: u32,
-}
+/// A safe, untyped, heap-allocated `Crate`.
+pub struct UntypedCrate(Box<dyn Any>);
 
 impl UntypedCrate {
     /// Creates a new [`UntypedCrate`] from a `Crate`.
-    pub fn new<T: Crate>(crate_: T) -> Self {
+    pub fn new<T: Any + 'static>(crate_: T) -> Self {
         let crate_ = Box::new(crate_);
-
-        Self {
-            crate_: Box::into_raw(crate_).cast::<()>(),
-            format_version: T::FORMAT_VERSION,
-        }
+        Self(crate_)
     }
 
     /// Downcasts an [`UntypedCrate`] into a concrete `Crate`.
@@ -26,15 +16,7 @@ impl UntypedCrate {
     ///
     /// If the format version of `Crate` is not equal to the format version of the
     /// [`UntypedCrate`].
-    pub fn into_crate<T: Crate>(self) -> Box<T> {
-        assert_eq!(
-            self.format_version,
-            T::FORMAT_VERSION,
-            "attempted to convert an untyped `Crate` with format version {} to typed `Crate` with format version {}",
-            self.format_version,
-            T::FORMAT_VERSION,
-        );
-
-        unsafe { Box::from_raw(self.crate_.cast::<T>()) }
+    pub fn into_crate<T: 'static>(self) -> Box<T> {
+        self.0.downcast().unwrap()
     }
 }
