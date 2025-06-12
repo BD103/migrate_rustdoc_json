7835b78d313a61782a2ce7ee2a5ba476cf8d9a32
BD103 <59022059+BD103@users.noreply.github.com>
2025-06-12 15:22:36 -0400

    refactor: make `MIGRATIONS` a `BTreeMap`
    Closes #3.

--- src/migrations/mod.rs
+++ src/migrations/mod.rs
@@ -1,6 +1,6 @@
 //! All migrations currently implemented by this crate.
 
-use std::any::Any;
+use std::{any::Any, collections::BTreeMap, sync::LazyLock};
 
 use anstream::eprintln;
 use anstyle::{AnsiColor, Color, Style};
@@ -15,221 +15,54 @@ type MigrateUpFn = fn(crate_: Box<dyn Any>) -> anyhow::Result<Box<dyn Any>>;
 type DeserializeFn = fn(&str) -> anyhow::Result<Box<dyn Any>>;
 type SerializeFn = fn(crate_: Box<dyn Any>) -> anyhow::Result<String>;
 
-/// A function lookup table for migrating Rustdoc JSON from one version to another.
-///
-/// `MIGRATIONS[0]` migrates v1 to v2, `MIGRATIONS[1]` migrates v2 to v3, etc.
-static MIGRATIONS: [(MigrateUpFn, DeserializeFn, SerializeFn); 45] = [
-    (
-        unimplemented_migrate_up::<1>,
-        unimplemented_deserialize::<1>,
-        unimplemented_serialize::<1>,
-    ),
-    (
-        unimplemented_migrate_up::<2>,
-        unimplemented_deserialize::<2>,
-        unimplemented_serialize::<2>,
-    ),
-    (
-        unimplemented_migrate_up::<3>,
-        unimplemented_deserialize::<3>,
-        unimplemented_serialize::<3>,
-    ),
-    (
-        unimplemented_migrate_up::<4>,
-        unimplemented_deserialize::<4>,
-        unimplemented_serialize::<4>,
-    ),
-    (
-        unimplemented_migrate_up::<5>,
-        unimplemented_deserialize::<5>,
-        unimplemented_serialize::<5>,
-    ),
-    (
-        unimplemented_migrate_up::<6>,
-        unimplemented_deserialize::<6>,
-        unimplemented_serialize::<6>,
-    ),
-    (
-        unimplemented_migrate_up::<7>,
-        unimplemented_deserialize::<7>,
-        unimplemented_serialize::<7>,
-    ),
-    (
-        unimplemented_migrate_up::<8>,
-        unimplemented_deserialize::<8>,
-        unimplemented_serialize::<8>,
-    ),
-    (
-        unimplemented_migrate_up::<9>,
-        unimplemented_deserialize::<9>,
-        unimplemented_serialize::<9>,
-    ),
-    (
-        unimplemented_migrate_up::<10>,
-        unimplemented_deserialize::<10>,
-        unimplemented_serialize::<10>,
-    ),
-    (
-        unimplemented_migrate_up::<11>,
-        unimplemented_deserialize::<11>,
-        unimplemented_serialize::<11>,
-    ),
-    (
-        unimplemented_migrate_up::<12>,
-        unimplemented_deserialize::<12>,
-        unimplemented_serialize::<12>,
-    ),
-    (
-        unimplemented_migrate_up::<13>,
-        unimplemented_deserialize::<13>,
-        unimplemented_serialize::<13>,
-    ),
-    (
-        unimplemented_migrate_up::<14>,
-        unimplemented_deserialize::<14>,
-        unimplemented_serialize::<14>,
-    ),
-    (
-        unimplemented_migrate_up::<15>,
-        unimplemented_deserialize::<15>,
-        unimplemented_serialize::<15>,
-    ),
-    (
-        unimplemented_migrate_up::<16>,
-        unimplemented_deserialize::<16>,
-        unimplemented_serialize::<16>,
-    ),
-    (
-        unimplemented_migrate_up::<17>,
-        unimplemented_deserialize::<17>,
-        unimplemented_serialize::<17>,
-    ),
-    (
-        unimplemented_migrate_up::<18>,
-        unimplemented_deserialize::<18>,
-        unimplemented_serialize::<18>,
-    ),
-    (
-        unimplemented_migrate_up::<19>,
-        unimplemented_deserialize::<19>,
-        unimplemented_serialize::<19>,
-    ),
-    (
-        unimplemented_migrate_up::<20>,
-        unimplemented_deserialize::<20>,
-        unimplemented_serialize::<20>,
-    ),
-    (
-        unimplemented_migrate_up::<21>,
-        unimplemented_deserialize::<21>,
-        unimplemented_serialize::<21>,
-    ),
-    (
-        unimplemented_migrate_up::<22>,
-        unimplemented_deserialize::<22>,
-        unimplemented_serialize::<22>,
-    ),
-    (
-        unimplemented_migrate_up::<23>,
-        unimplemented_deserialize::<23>,
-        unimplemented_serialize::<23>,
-    ),
-    (
-        unimplemented_migrate_up::<24>,
-        unimplemented_deserialize::<24>,
-        unimplemented_serialize::<24>,
-    ),
-    (
-        unimplemented_migrate_up::<25>,
-        unimplemented_deserialize::<25>,
-        unimplemented_serialize::<25>,
-    ),
-    (
-        unimplemented_migrate_up::<26>,
-        unimplemented_deserialize::<26>,
-        unimplemented_serialize::<26>,
-    ),
-    (
-        unimplemented_migrate_up::<27>,
-        unimplemented_deserialize::<27>,
-        unimplemented_serialize::<27>,
-    ),
-    (
-        unimplemented_migrate_up::<28>,
-        unimplemented_deserialize::<28>,
-        unimplemented_serialize::<28>,
-    ),
-    (
-        unimplemented_migrate_up::<29>,
-        unimplemented_deserialize::<29>,
-        unimplemented_serialize::<29>,
-    ),
-    (
-        unimplemented_migrate_up::<30>,
-        unimplemented_deserialize::<30>,
-        unimplemented_serialize::<30>,
-    ),
-    (
-        unimplemented_migrate_up::<31>,
-        unimplemented_deserialize::<31>,
-        unimplemented_serialize::<31>,
-    ),
-    (
-        unimplemented_migrate_up::<32>,
-        unimplemented_deserialize::<32>,
-        unimplemented_serialize::<32>,
-    ),
-    (
-        unimplemented_migrate_up::<33>,
-        unimplemented_deserialize::<33>,
-        unimplemented_serialize::<33>,
-    ),
-    (
-        unimplemented_migrate_up::<34>,
-        unimplemented_deserialize::<34>,
-        unimplemented_serialize::<34>,
-    ),
-    (
-        unimplemented_migrate_up::<35>,
-        unimplemented_deserialize::<35>,
-        unimplemented_serialize::<35>,
-    ),
-    (
-        unimplemented_migrate_up::<36>,
-        unimplemented_deserialize::<36>,
-        unimplemented_serialize::<36>,
-    ),
-    (
-        unimplemented_migrate_up::<37>,
-        unimplemented_deserialize::<37>,
-        unimplemented_serialize::<37>,
-    ),
-    (
-        unimplemented_migrate_up::<38>,
-        unimplemented_deserialize::<38>,
-        unimplemented_serialize::<38>,
-    ),
-    (
-        unimplemented_migrate_up::<39>,
-        unimplemented_deserialize::<39>,
-        unimplemented_serialize::<39>,
-    ),
-    (
-        unimplemented_migrate_up::<40>,
-        unimplemented_deserialize::<40>,
-        unimplemented_serialize::<40>,
-    ),
-    (v41::migrate_up, v41::deserialize, v41::serialize),
-    (v42::migrate_up, v42::deserialize, v42::serialize),
-    (v43::migrate_up, v43::deserialize, v43::serialize),
-    (v44::migrate_up, v44::deserialize, v44::serialize),
-    (
-        // v46 does not exist yet, so we cannot migrate past v45.
-        unimplemented_migrate_up::<45>,
-        v45::deserialize,
-        v45::serialize,
-    ),
-];
+static MIGRATIONS: LazyLock<BTreeMap<u32, (MigrateUpFn, DeserializeFn, SerializeFn)>> =
+    LazyLock::new(|| {
+        let migrations = [
+            (
+                41,
+                (
+                    v41::migrate_up as MigrateUpFn,
+                    v41::deserialize as DeserializeFn,
+                    v41::serialize as SerializeFn,
+                ),
+            ),
+            (
+                42,
+                (
+                    v42::migrate_up as MigrateUpFn,
+                    v42::deserialize as DeserializeFn,
+                    v42::serialize as SerializeFn,
+                ),
+            ),
+            (
+                43,
+                (
+                    v43::migrate_up as MigrateUpFn,
+                    v43::deserialize as DeserializeFn,
+                    v43::serialize as SerializeFn,
+                ),
+            ),
+            (
+                44,
+                (
+                    v44::migrate_up as MigrateUpFn,
+                    v44::deserialize as DeserializeFn,
+                    v44::serialize as SerializeFn,
+                ),
+            ),
+            (
+                45,
+                (
+                    // v46 does not exist yet, so we cannot migrate past v45.
+                    unimplemented_migrate_up::<45> as MigrateUpFn,
+                    v45::deserialize as DeserializeFn,
+                    v45::serialize as SerializeFn,
+                ),
+            ),
+        ];
+
+        BTreeMap::from(migrations)
+    });
 
 pub fn migrate_up(current: &str, to_version: u32) -> anyhow::Result<String> {
     let current_version = crate::version::detect_version(current)?;
@@ -241,7 +74,7 @@ pub fn migrate_up(current: &str, to_version: u32) -> anyhow::Result<String> {
         .context("downgrading to an older format version is not supported"));
     }
 
-    let deserialize = MIGRATIONS[current_version as usize - 1].1;
+    let deserialize = MIGRATIONS[&current_version].1;
 
     eprintln!(
         "{blue}Migrating JSON with format version {bold}v{current_version}{bold:#}{blue:#}",
@@ -253,7 +86,7 @@ pub fn migrate_up(current: &str, to_version: u32) -> anyhow::Result<String> {
     let mut crate_ = (deserialize)(current)?;
 
     for i in current_version..to_version {
-        let migrate_up = MIGRATIONS[i as usize - 1].0;
+        let migrate_up = MIGRATIONS[&i].0;
 
         eprintln!(
             "\t{dim}...to{dim:#} {blue}v{version}{blue:#}",
@@ -270,7 +103,7 @@ pub fn migrate_up(current: &str, to_version: u32) -> anyhow::Result<String> {
         crate_ = (migrate_up)(crate_)?;
     }
 
-    let serialize = MIGRATIONS[to_version as usize - 1].2;
+    let serialize = MIGRATIONS[&to_version].2;
 
     // Convert the `UntypedCrate` back to a JSON string.
     (serialize)(crate_)
@@ -287,24 +120,4 @@ fn unimplemented_migrate_up<const N: usize>(_: Box<dyn Any>) -> anyhow::Result<B
     }
 
     inner(N)
-}
-
-/// Errors when called, displaying an error that deserialization isn't yet supported.
-fn unimplemented_deserialize<const N: usize>(_: &str) -> anyhow::Result<Box<dyn Any>> {
-    fn inner(n: usize) -> anyhow::Result<Box<dyn Any>> {
-        Err(anyhow::anyhow!("format version v{n} is not yet supported"))
-    }
-
-    inner(N)
-}
-
-/// Panics when called, displaying an error that serialization should be supported but isn't.
-fn unimplemented_serialize<const N: usize>(_: Box<dyn Any>) -> anyhow::Result<String> {
-    fn inner(n: usize) -> String {
-        // This is unreachable because if we support migrating to this version, we should support
-        // serializing it as well.
-        unreachable!("unable to convert `Crate` format version v{n} to JSON");
-    }
-
-    Ok(inner(N))
 }
