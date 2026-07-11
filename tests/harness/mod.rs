23fd2271927efe58938f7d42e878547a20111332
BD103 <59022059+BD103@users.noreply.github.com>
2026-07-11 16:44:26 -0400

    chore: migration test harness

---
+++ tests/harness/mod.rs
@@ -0,0 +1,84 @@
+use std::{ops::ControlFlow, path::PathBuf};
+
+use jsonpath_rust::JsonPath;
+
+use crate::utils::{generate_and_migrate_to, needs_toolchain};
+
+/// A builder that configures and runs a migration test.
+///
+/// The Rust file used to generate the Rustdoc JSON is assumed to be stored at `tests/vXX/vXX.rs`,
+/// where `XX` is the current format version passed to [`MigrationTest::new()`].
+pub(crate) struct MigrationTest {
+    current: u32,
+    up: u32,
+    path: PathBuf,
+    queries: Vec<Query>,
+}
+
+impl MigrationTest {
+    /// Creates a new migration test.
+    ///
+    /// `current` is the format version of the original Rustdoc JSON, and `up` is the format
+    /// version that JSON will be migrated to using `migrate_rustdoc_json`.
+    pub(crate) fn new(current: u32, up: u32) -> Self {
+        Self {
+            current,
+            up,
+            path: PathBuf::from_iter(["tests", &format!("v{current}"), &format!("v{current}.rs")]),
+            queries: Vec::with_capacity(1),
+        }
+    }
+
+    /// Adds a new query test.
+    ///
+    /// `query` is the `JSONPath` querying both the original and migrated Rustdoc JSON.
+    /// `current_expected` is the expected query result on the original JSON, and `up_expected` is
+    /// the same for the migrated JSON.
+    pub(crate) fn query(
+        mut self,
+        query: &'static str,
+        current_expected: serde_json::Value,
+        up_expected: serde_json::Value,
+    ) -> Self {
+        self.queries.push(Query {
+            query,
+            current_expected,
+            up_expected,
+        });
+        self
+    }
+
+    /// Executes this migration test.
+    pub(crate) fn test(self) {
+        let ControlFlow::Continue(()) = needs_toolchain(self.current) else {
+            return;
+        };
+
+        let (current_json, up_json) = generate_and_migrate_to(self.path, self.current, self.up);
+
+        for Query {
+            query,
+            current_expected,
+            up_expected,
+        } in self.queries
+        {
+            let current_results = current_json.query(query).unwrap();
+            let up_results = up_json.query(query).unwrap();
+
+            for current_result in current_results {
+                assert_eq!(*current_result, current_expected);
+            }
+
+            for up_result in up_results {
+                assert_eq!(*up_result, up_expected);
+            }
+        }
+    }
+}
+
+/// A single JSONPath query with expected current and up results.
+struct Query {
+    query: &'static str,
+    current_expected: serde_json::Value,
+    up_expected: serde_json::Value,
+}
--- tests/migrate.rs
+++ tests/migrate.rs
@@ -1,81 +1,64 @@
+mod harness;
 mod utils;
 
 use std::ops::ControlFlow;
 
-use jsonpath_rust::JsonPath;
 use serde_json::{Value, json};
 use utils::{generate_and_migrate_to, needs_toolchain, query_both};
 
+use self::harness::MigrationTest;
+
 #[test]
 fn v42() {
-    let ControlFlow::Continue(()) = needs_toolchain(42) else {
-        return;
-    };
-
-    let (source_json, migrated_json) = generate_and_migrate_to("tests/v42/v42.rs", 42, 43);
-
-    let query_expected = [
-        (
+    MigrationTest::new(42, 43)
+        .query(
             "$.index[?(@.name == 'ReprRust')].attrs",
             json!(["#[attr = Repr([ReprRust])]\n"]),
             json!(["#[repr(Rust)]"]),
-        ),
-        (
+        )
+        .query(
             "$.index[?(@.name == 'ReprC')].attrs",
             json!(["#[attr = Repr([ReprC])]\n"]),
             json!(["#[repr(C)]"]),
-        ),
-        (
+        )
+        .query(
             "$.index[?(@.name == 'ReprPacked1')].attrs",
             json!(["#[attr = Repr([ReprPacked(Align(1 bytes))])]\n"]),
             json!(["#[repr(packed(1))]"]),
-        ),
-        (
+        )
+        .query(
             "$.index[?(@.name == 'ReprPacked2')].attrs",
             json!(["#[attr = Repr([ReprPacked(Align(2 bytes))])]\n"]),
             json!(["#[repr(packed(2))]"]),
-        ),
-        (
+        )
+        .query(
             "$.index[?(@.name == 'ReprCAlign8')].attrs",
             json!(["#[attr = Repr([ReprC, ReprAlign(Align(8 bytes))])]\n"]),
             json!(["#[repr(C, align(8))]"]),
-        ),
-        (
+        )
+        .query(
             "$.index[?(@.name == 'ReprI8')].attrs",
             json!(["#[attr = Repr([ReprInt(SignedInt(I8))])]\n"]),
             json!(["#[repr(i8)]"]),
-        ),
-        (
+        )
+        .query(
             "$.index[?(@.name == 'ReprUsizeC')].attrs",
             json!(["#[attr = Repr([ReprInt(UnsignedInt(Usize)), ReprC])]\n"]),
             json!(["#[repr(usize, C)]"]),
-        ),
-        (
+        )
+        .query(
             "$.index[?(@.name == 'TransparentPub')].attrs",
             json!(["#[attr = Repr([ReprTransparent])]\n"]),
             json!(["#[repr(transparent)]"]),
-        ),
-        (
+        )
+        .query(
             "$.index[?(@.name == 'TransparentPriv')].attrs",
             json!(["#[attr = Repr([ReprTransparent])]\n"]),
             // Although Rustdoc would hide this `#[repr(transparent)]` if the JSON was built in
             // v43, the migration doesn't yet have the logic to detect this.
             json!(["#[repr(transparent)]"]),
-        ),
-    ];
-
-    for (query, source_expected, migrated_expected) in query_expected {
-        let source_results = source_json.query(query).unwrap();
-        let migrated_results = migrated_json.query(query).unwrap();
-
-        for actual in source_results {
-            assert_eq!(*actual, source_expected);
-        }
-
-        for actual in migrated_results {
-            assert_eq!(*actual, migrated_expected);
-        }
-    }
+        )
+        .test();
 }
 
 #[test]
@@ -119,160 +102,85 @@ fn v44() {
 
 #[test]
 fn v45() {
-    let ControlFlow::Continue(()) = needs_toolchain(45) else {
-        return;
-    };
-
-    let (_, migrated_json) = generate_and_migrate_to("tests/v45/v45.rs", 45, 46);
-
-    let pub_query = "$.index[?(@.name == 'TransparentPub')].attrs";
-    let pub_expected = json!(["#[repr(transparent)]"]);
-
-    let priv_query = "$.index[?(@.name == 'TransparentPriv')].attrs";
-    let priv_expected = json!([]);
-
-    for result in migrated_json.query(pub_query).unwrap() {
-        assert_eq!(result, &pub_expected);
-    }
-
-    for result in migrated_json.query(priv_query).unwrap() {
-        assert_eq!(result, &priv_expected);
-    }
+    MigrationTest::new(45, 46)
+        .query(
+            "$.index[?(@.name == 'TransparentPub')].attrs",
+            json!(["#[repr(transparent)]"]),
+            json!(["#[repr(transparent)]"]),
+        )
+        .query(
+            "$.index[?(@.name == 'TransparentPriv')].attrs",
+            json!([]),
+            json!([]),
+        )
+        .test();
 }
 
 #[test]
 fn v46() {
-    let ControlFlow::Continue(()) = needs_toolchain(46) else {
-        return;
-    };
-
-    let (source_json, migrated_json) = generate_and_migrate_to("tests/v46/v46.rs", 46, 48);
-
-    let query_expected = [
-        (
+    MigrationTest::new(46, 48)
+        .query(
             "$.index[?(@.name == 'inline_hint')].attrs",
             json!(["#[inline]"]),
             json!(["#[attr = Inline(Hint)]"]),
-        ),
-        (
+        )
+        .query(
             "$.index[?(@.name == 'inline_always')].attrs",
             json!(["#[inline(always)]"]),
             json!(["#[attr = Inline(Always)]"]),
-        ),
-        (
+        )
+        .query(
             "$.index[?(@.name == 'inline_never')].attrs",
             json!(["#[inline(never)]"]),
             json!(["#[attr = Inline(Never)]"]),
-        ),
-    ];
-
-    for (query, source_expected, migrated_expected) in query_expected {
-        let source_results = source_json.query(query).unwrap();
-        let migrated_results = migrated_json.query(query).unwrap();
-
-        for actual in source_results {
-            assert_eq!(*actual, source_expected);
-        }
-
-        for actual in migrated_results {
-            assert_eq!(*actual, migrated_expected);
-        }
-    }
+        )
+        .test();
 }
 
 #[test]
 fn v48() {
-    let ControlFlow::Continue(()) = needs_toolchain(48) else {
-        return;
-    };
-
-    let (source_json, migrated_json) = generate_and_migrate_to("tests/v48/v48.rs", 48, 49);
-
-    let query_expected = [
-        (
+    MigrationTest::new(48, 49)
+        .query(
             "$.index[?(@.name == 'do_not_optimize')].attrs",
             json!(["#[optimize(none)]"]),
             json!(["#[attr = Optimize(DoNotOptimize)]"]),
-        ),
-        (
+        )
+        .query(
             "$.index[?(@.name == 'optimize_speed')].attrs",
             json!(["#[optimize(speed)]"]),
             json!(["#[attr = Optimize(Speed)]"]),
-        ),
-        (
+        )
+        .query(
             "$.index[?(@.name == 'optimize_size')].attrs",
             json!(["#[optimize(size)]"]),
             json!(["#[attr = Optimize(Size)]"]),
-        ),
-    ];
-
-    for (query, source_expected, migrated_expected) in query_expected {
-        let source_results = source_json.query(query).unwrap();
-        let migrated_results = migrated_json.query(query).unwrap();
-
-        for actual in source_results {
-            assert_eq!(*actual, source_expected);
-        }
-
-        for actual in migrated_results {
-            assert_eq!(*actual, migrated_expected);
-        }
-    }
+        )
+        .test();
 }
 
 #[test]
 fn v49() {
-    let ControlFlow::Continue(()) = needs_toolchain(49) else {
-        return;
-    };
-
-    let (source_json, migrated_json) = generate_and_migrate_to("tests/v49/v49.rs", 49, 50);
-
-    let query = "$.index[?(@.name == 'cold')].attrs";
-
-    let source_results = source_json.query(query).unwrap();
-    let migrated_results = migrated_json.query(query).unwrap();
-
-    for actual in source_results {
-        assert_eq!(*actual, json!(["#[cold]"]));
-    }
-
-    for actual in migrated_results {
-        assert_eq!(*actual, json!(["#[attr = Cold]"]));
-    }
+    MigrationTest::new(49, 50)
+        .query(
+            "$.index[?(@.name == 'cold')].attrs",
+            json!(["#[cold]"]),
+            json!(["#[attr = Cold]"]),
+        )
+        .test();
 }
 
 #[test]
 fn v51() {
-    let ControlFlow::Continue(()) = needs_toolchain(51) else {
-        return;
-    };
-
-    let (source_json, migrated_json) = generate_and_migrate_to("tests/v51/v51.rs", 51, 52);
-
-    let query_expected = [
-        (
+    MigrationTest::new(51, 52)
+        .query(
             "$.index[?(@.name == 'without_message')].attrs",
             json!(["#[must_use]"]),
             json!(["#[attr = MustUse]"]),
-        ),
-        (
+        )
+        .query(
             "$.index[?(@.name == 'with_message')].attrs",
             json!(["#[must_use = \"custom message\"]"]),
             json!(["#[attr = MustUse {reason: \"custom message\"}]"]),
-        ),
-    ];
-
-    for (query, source_expected, migrated_expected) in query_expected {
-        let source_results = source_json.query(query).unwrap();
-        let migrated_results = migrated_json.query(query).unwrap();
-
-        for actual in source_results {
-            assert_eq!(*actual, source_expected);
-        }
-
-        for actual in migrated_results {
-            assert_eq!(*actual, migrated_expected);
-        }
-    }
+        )
+        .test();
 }
