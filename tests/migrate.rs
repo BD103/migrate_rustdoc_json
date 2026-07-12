fb4d377cb47d0187ac4ece2f5d8c287211209dbf
BD103 <59022059+BD103@users.noreply.github.com>
2026-07-12 17:58:44 -0400

    Revert "fix: make `v43_to_v44` a migration test using customized query test"
    This reverts commit f8efd944487e1ab522dfff1f7bb08670a226e70f.

--- tests/migrate.rs
+++ tests/migrate.rs
@@ -3,7 +3,6 @@ mod utils;
 
 use std::ops::ControlFlow;
 
-use jsonpath_rust::query::QueryRef;
 use serde_json::{Value, json};
 use utils::{generate_and_migrate_to, needs_toolchain, query_both};
 
@@ -64,36 +63,19 @@ fn v42_to_v43() {
 
 #[test]
 fn v43_to_v44() {
-    MigrationTest::new(43, 44)
-        .query_custom(
-            "$.target",
-            Box::new(|result: QueryRef<'_, Value>| {
-                assert_eq!(
-                    result.clone().val(),
-                    &json!(null),
-                    "original result does not match expected value, query $.target at path {path}",
-                    path = result.path(),
-                );
-            }),
-            Box::new(|result: QueryRef<'_, Value>| {
-                let val = result.val();
+    let ControlFlow::Continue(()) = needs_toolchain(43) else {
+        return;
+    };
 
-                assert!(val["target_features"].is_array());
-                assert!(val["triple"].is_string());
-            }),
-            Box::new(|result: QueryRef<'_, Value>| {
-                assert_eq!(
-                    result.clone().val(),
-                    &json!({
-                        "target_features": [],
-                        "triple": "",
-                    }),
-                    "original result does not match expected value, query $.target at path {path}",
-                    path = result.path(),
-                );
-            }),
-        )
-        .test();
+    let GeneratedAndMigrated { migrated_json, .. } =
+        generate_and_migrate_to("tests/migrations/v43_to_v44.rs", 43, 44);
+
+    let expected = json!({
+        "triple": "",
+        "target_features": [],
+    });
+
+    assert_eq!(migrated_json["target"], expected);
 }
 
 #[test]
